03e3f16302f3b7819f9cb604c2640505edb13a2f
PaulaSousa2014 <110301198+PaulaSousa2014@users.noreply.github.com>
2022-10-10 15:45:31 +0200

    Iteration1

---
+++ labSem3dia1/src/components/Composition.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>New Component on Composition</h1>
+</template>
+<script></script>
+<style></style>
---
+++ labSem3dia1/src/components/Option.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>New Component on Option</h1>
+</template>
+<script></script>
+<style></style>
